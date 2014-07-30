f894a71721044b6027cde9a9e6df5f83c888edf9
Olivier Goutay <olivier.goutay@free.fr>
2014-07-30 09:47:06 +0200

    Added robotium tests (example)

--- app/src/androidTest/java/com/og/encryptedsharedpreferencesexample/ApplicationTest.java
+++ app/src/androidTest/java/com/og/encryptedsharedpreferencesexample/ApplicationTest.java
@@ -1,17 +1,38 @@
 package com.og.encryptedsharedpreferencesexample;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+import com.robotium.solo.Solo;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MyActivity> {
+    private Solo solo;
 
     /**
      * Constructor
      */
     public ApplicationTest() {
-        super(Application.class);
+        super(MyActivity.class);
+    }
+
+    /**
+     * Set up
+     * @throws Exception if one
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    /**
+     * Main test
+     */
+    public void testActivity() {
+        // robotium assert
+        solo.assertCurrentActivity("Main activity", MyActivity.class);
+        // junit assert
+        assertEquals(true, true);
     }
 }
